7090bf4b377984963773c04f646fc7e81bc45064
Adam Coddington <me@adamcoddington.net>
2017-06-15 23:57:04 -0700

    Updating USB device name.

--- firmware/lib/usbdrv/usbconfig.h
+++ firmware/lib/usbdrv/usbconfig.h
@@ -145,7 +145,7 @@ extern void usbEventResetReady(void);
  * obdev's free shared VID/PID pair. See the file USBID-License.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'A', 'z', 'b', 'u', 'k', 'a', ' ', '1', '.', '0'
+#define USB_CFG_DEVICE_NAME     'A', 'z', 'b', 'u', 'k', 'a', ' ', '3', '.', '0'
 #define USB_CFG_DEVICE_NAME_LEN 10
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USBID-License.txt before you assign a name if you
